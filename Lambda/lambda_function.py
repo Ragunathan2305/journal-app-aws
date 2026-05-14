caddf65078445c78809384909270b90d2009dd8c
Ragunathan K <76608965+Ragunathan2305@users.noreply.github.com>
2026-05-14 17:20:36 +0530

    Add lambda.py

---
+++ Lambda/lambda_function.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+from datetime import datetime
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('JournalEntries')
+
+def lambda_handler(event, context):
+    
+    http_method = event['httpMethod']
+    
+    # SAVE a new journal entry
+    if http_method == 'POST':
+        body = json.loads(event['body'])
+        entry_text = body['entryText']
+        
+        table.put_item(Item={
+            'UserID': 'user_001',
+            'EntryTimestamp': datetime.now().isoformat(),
+            'EntryText': entry_text
+        })
+        
+        return {
+            'statusCode': 200,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': json.dumps('Entry saved successfully!')
+        }
+    
+    # GET all journal entries
+    elif http_method == 'GET':
+        response = table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('UserID').eq('user_001')
+        )
+        
+        return {
+            'statusCode': 200,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': json.dumps(response['Items'])
+        }
